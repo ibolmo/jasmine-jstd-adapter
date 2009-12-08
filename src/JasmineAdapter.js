475873a3554b0d02cc1de7fd950a5e416b838927
Olmo Maldonado <olmo@G5.local>
2009-12-08 14:14:38 -0600

    init commit

---
+++ src/JasmineAdapter.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview Jasmine JsTestDriver Adapter.
+ * @author ibolmo@gmail.com (Olmo Maldonado)
+ */
+
+jasmine.Env.prototype.it = function(desc, func){
+	
+};
+
+jasmine.Env.prototype.xit = function(desc, func) {
+	
+};
+
+jasmine.Env.prototype.describe = function(description, specDefinitions) {
+	
+};
+
