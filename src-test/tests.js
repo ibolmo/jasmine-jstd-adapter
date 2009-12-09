b76c6e87dca265119942036eea4e15ec4a4c6cc0
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-12-09 17:34:12 -0600

    some cleanup and added support for beforeEach and afterEach for individual specs

--- src-test/tests.js
+++ src-test/tests.js
@@ -1,12 +1,31 @@
 
 describe('Example', function(){
 
+	var increment = 0;
+	var list = [];
+
+	beforeEach(function(){
+		increment++;
+	});
+
+	afterEach(function(){
+		list.push(0);
+	});
+
 	it('should pass', function(){
 		expect(0).toBe(0);
 	});
 
 	it('should fail', function(){
 		expect(0).toBe(1);
+	});
+
+	it('beforeEach should have incremented', function(){
+		expect(increment).toBe(3);
+	});
+
+	it('afterEach should have appended an element to the list', function(){
+		expect(list.length).toBe(3);
 	});
 
 });
--- src/JasmineAdapter.js
+++ src/JasmineAdapter.js
@@ -3,8 +3,10 @@
  * @author ibolmo@gmail.com (Olmo Maldonado)
  */
 
+
 jasmine.Env.prototype.describe = (function(describe){
 
+	// TODO(ibolmo): Support nested describes.
 	return function(description, specDefinitions){
 		this.currentTestCase = TestCase(description);
 		return describe.call(this, description, specDefinitions);
@@ -23,16 +25,38 @@ jasmine.Env.prototype.it = (function(it){
 
 })(jasmine.Env.prototype.it);
 
+
+jasmine.Env.prototype.beforeEach = (function(beforeEach){
+
+	// TODO(ibolmo): Support beforeEach TestCase.
+	return function(beforeEachFunction) {
+		beforeEach.call(this, beforeEachFunction);
+		this.currentTestCase.prototype.setUp = beforeEachFunction;
+	};
+
+})(jasmine.Env.prototype.beforeEach);
+
+
+jasmine.Env.prototype.afterEach = (function(afterEach){
+
+	// TODO(ibolmo): Support afterEach TestCase.
+	return function(afterEachFunction) {
+		afterEach.call(this, afterEachFunction);
+		this.currentTestCase.prototype.tearDown = afterEachFunction;
+	};
+
+})(jasmine.Env.prototype.afterEach);
+
+
 jasmine.NestedResults.prototype.addResult = (function(addResult){
 
 	return function(result) {
 		addResult.call(this, result);
-		if (result.type != 'MessageResult') {
-			if (!result.passed()) fail(result.message);
-		}
+		if (result.type != 'MessageResult' && !result.passed()) fail(result.message);
 	};
 
 })(jasmine.NestedResults.prototype.addResult);
+
 
 // Reset environment with overriden methods.
 jasmine.currentEnv_ = null;
